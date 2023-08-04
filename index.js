b0e1d39bfc54646d3f037dd346c5eae2b5e7fc60
NamVT17 <namthanhvu2002nb@gmail.com>
2023-08-04 22:09:01 +0700

    Rest API

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const db = require('./queries')
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (request, response) => {
+  response.json({ info: 'Hello World' })
 })
+
+
+app.get('/register', db.getUsers)
+app.get('/register/:id', db.getUserById)
+app.post('/register', db.createUser)
+app.put('/register/:id', db.changePass)
+app.delete('/register/:id', db.deleteUser)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
